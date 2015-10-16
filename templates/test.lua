7ffc6084f49c2442efb23524df24a7fefd82bcf4
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2015-10-16 17:59:21 +0200

    Use the bootpins module to read the data.

--- templates/test.lua
+++ templates/test.lua
@@ -1,5 +1,6 @@
 module("@MODULE_NAME@", package.seeall)
 
+require("bootpins")
 require("parameters")
 
 CFG_strTestName = "@TEST_NAME@"
@@ -52,43 +53,20 @@ function run(aParameters)
 		error("No plug-in selected, nothing to do!")
 	end
 
-	-- Get the binary for the ASIC.
-	local tAsicTyp = tPlugin:GetChiptyp()
-	if tAsicTyp==romloader.ROMLOADER_CHIPTYP_NETX50 then
-		uiAsic = 50
-	elseif tAsicTyp==romloader.ROMLOADER_CHIPTYP_NETX100 or tAsicTyp==romloader.ROMLOADER_CHIPTYP_NETX500 then
-		uiAsic = 500
-	elseif tAsicTyp==romloader.ROMLOADER_CHIPTYP_NETX10 then
-		uiAsic = 10
-	elseif tAsicTyp==romloader.ROMLOADER_CHIPTYP_NETX56 or tAsicTyp==romloader.ROMLOADER_CHIPTYP_NETX56B then
-		uiAsic = 56
-	else
-		error("Unknown chiptyp!")
-	end
-	local strNetxBinary = string.format("netx/bootpins_netx%d.bin", uiAsic)
-
-	-- Download the binary, execute it and get the results back.
-	local aParameter = {
-		"OUTPUT",
-		"OUTPUT",
-		"OUTPUT"
-	}
-	local ulResult = tester.mbin_simple_run(nil, tPlugin, strNetxBinary, aParameter)
-	if ulResult~=0 then
-		error("The test failed with return code:" .. ulResult)
-	end
+	-- Read the bootpins.
+	atBootpins = bootpins.read(tPlugin)
 
 	-- Compare the data with the expected values.
 	local fOk = true
-	if ulExpectedBootMode~=aParameter[1] then
+	if ulExpectedBootMode~=aBootpins.boot_mode then
 		print(string.format("ERROR: the expected boot mode is 0x%08x, but 0x%08x was detected.", ulExpectedBootMode, aParameter[1]))
 		fOk = false
 	end
-	if ulExpectedStrappingOptions~=aParameter[2] then
+	if ulExpectedStrappingOptions~=aBootpins.strapping_options then
 		print(string.format("ERROR: the expected strapping options are 0x%08x, but 0x%08x was detected.", ulExpectedStrappingOptions, aParameter[2]))
 		fOk = false
 	end
-	if ulExpectedChipId~=aParameter[1] then
+	if ulExpectedChipId~=aBootpins.chip_id then
 		print(string.format("ERROR: the expected chip ID is 0x%08x, but 0x%08x was detected.", ulExpectedChipId, aParameter[3]))
 		fOk = false
 	end
