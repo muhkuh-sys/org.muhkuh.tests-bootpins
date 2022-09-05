82c1abc11ab3534c7b0fd78785f979ed89cdf819
Stephan Lesch <slesch@hilscher.com>
2022-09-05 15:54:18 +0200

    [code] Add a note "for CLI Flasher" to version message.

--- bootpins/src/main_test.c
+++ bootpins/src/main_test.c
@@ -642,7 +642,7 @@ TEST_RESULT_T test_main(TEST_PARAMETER_T *ptTestParam)
 	systime_init();
 
 	uprintf("\f. *** Bootpins test by doc_bacardi@users.sourceforge.net ***\n");
-	uprintf("V" VERSION_ALL "\n\n");
+	uprintf("V" VERSION_ALL " (for CLI Flasher) \n\n");
 
 	/* Switch off SYS led. */
 	rdy_run_setLEDs(RDYRUN_OFF);
